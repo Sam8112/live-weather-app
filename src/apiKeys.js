3a676ba648f8a0a41843068ca60a446e3444902c
Sameer Sharma <118039352+Sam8112@users.noreply.github.com>
2024-07-24 18:02:41 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,6 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
+// apiKeys.js
 module.exports = {
-  key: "5593335cfcf92c81507f7d430a896808",
+  key: process.env.REACT_APP_API_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
+
